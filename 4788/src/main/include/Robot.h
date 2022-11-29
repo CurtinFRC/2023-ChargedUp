f21a3bf6fda12a1bdef2549ec6231264c60cc280
Anna <anna.pedersen@student.education.wa.edu.au>
2022-11-29 13:12:28 +0800

    PID Wasp method

--- 4788/src/main/include/Robot.h
+++ 4788/src/main/include/Robot.h
@@ -4,11 +4,17 @@
 
 #include <frc/TimedRobot.h>
 #include <frc/smartdashboard/SendableChooser.h>
-// #include "startup.h"
-// #include "RobotStart.h"
-// #include "RobotStartup.h"
-
+#include <frc/XboxController.h>
+#include <iostream>
 #include <ctre/Phoenix.h>
+
+#include <frc/I2C.h>
+#include <frc/SPI.h>
+#include <frc/interfaces/Gyro.h>
+#include "AHRS.h"
+
+using namespace frc;
+
 
 // #include <frc/RobotBase.h>
 
@@ -26,4 +32,28 @@ class Robot : public frc::TimedRobot {
   void TestPeriodic() override;
 
  private:
+  frc::XboxController *driver;
+  TalonSRX *dropWheelOne, *dropWheelTwo;
+  TalonFX *leftDrive, *rightDrive;
+
+  // wml::sensors::NavX navX{frc::SPI::Port::kMXP, 200};
+  // wml::sensors::NavXGyro gyro{navX.Angular(wml::sensors::AngularAxis::YAW)};
+
+  // robotGyro gyro{SPI::Port::kMXP};
+  AHRS *ahrs;
+
+  bool dropToggle = false;
+  bool normalDrive = true;
+  double dropSpeed = 0;
+
+  double dropPIDSpeed = 0;
+
+  double targetAngle = 0;
+
+  double kP = 0.0004;
+  double kI = 0.000;
+  double kD = 0;
+
+  double _sum = 0;
+  double _previousError = 0;
 };
