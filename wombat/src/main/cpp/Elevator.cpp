620fbca1def33a71e04d055514134e751e5307cc
Anna <anna.pedersen@student.education.wa.edu.au>
2022-12-07 23:12:57 +0800

    added elevator unit error

---
+++ wombat/src/main/cpp/Elevator.cpp
@@ -0,0 +1,69 @@
+#include "Elevator.h"
+#include <networktables/NetworkTableInstance.h>
+#include <iostream>
+
+using namespace wom;
+
+Elevator::Elevator(ElevatorParams params)
+  : _params(params), _state(ElevatorState::kIdle),
+  _pid{params.pid},
+  _table(nt::NetworkTableInstance::GetDefault().GetTable("elevator")) {}
+
+
+void Elevator::OnUpdate(units::second_t dt) {
+  units::volt_t voltage{0};
+
+  units::meter_t height = (_params.gearbox.encoder->GetEncoderPosition() / _params.gearbox.reduction).value() * _params.radius;
+
+  switch(_state) {
+    case ElevatorState::kIdle:
+      voltage = 0_V;
+      break;
+    case ElevatorState::kManual:
+      voltage = _setpointManual;
+      break;
+    case ElevatorState::kPID:
+      {
+        auto feedforward = _params.gearbox.motor.Voltage((_params.mass * 9.81_m / (1_s * 1_s)) / _params.radius, units::radians_per_second_t{0});
+        voltage = _pid.Calculate(height, dt, feedforward);
+      }
+      break;
+    case ElevatorState::kZeroing:
+      if (!_params.bottomSensor) {
+        std::cout << "No bottom sensor found, can't zero" << std::endl;
+        _state = ElevatorState::kIdle;
+      } else {
+        voltage = -3_V;
+        if (_params.bottomSensor->Get()) {
+          _params.gearbox.encoder->ZeroEncoder();
+          _state = ElevatorState::kIdle;
+        }
+      }
+      break;
+  }
+
+  if (_params.bottomSensor && voltage < 0_V && _params.bottomSensor->Get()) {
+    voltage = 0_V;
+  } 
+  if (_params.topSensor && voltage > 0_V && _params.topSensor->Get()) {
+    voltage = 0_V;
+  }
+  _params.gearbox.transmission->SetVoltage(voltage);
+}
+
+void Elevator::SetManual(units::volt_t voltage) {
+  _state = ElevatorState::kManual;
+  _setpointManual = voltage;
+}
+
+void Elevator::SetPID() {
+  _state = ElevatorState::kPID;
+}
+
+void Elevator::SetZeroing() {
+  _state = ElevatorState::kZeroing;
+}
+
+void Elevator::SetIdle() {
+  _state = ElevatorState::kIdle;
+}
