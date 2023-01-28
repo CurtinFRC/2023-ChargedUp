a33a7885cdebb8017047db924bacf90f93e3d5e0
Darcey Saul <darceysaul@outlook.com>
2023-01-28 15:16:20 +0800

    Set for testing on protobot

--- src/main/cpp/behaviour/ArmavatorBehaviour.cpp
+++ src/main/cpp/behaviour/ArmavatorBehaviour.cpp
@@ -56,50 +56,50 @@ void ArmavatorGoToPositionBehaviour::OnTick(units::second_t dt) {
 }
 
 
-ArmavatorManualBehaviour::ArmavatorManualBehaviour(Armavator *armavator, frc::XboxController &codriver)
-: _armavator(armavator), _codriver(codriver) {
-  //tells code that the points are controlled (one point at a time) 
-  _setpoint.height = 0.0_m;
-  _setpoint.angle = 0.0_deg;
-  Controls(armavator);
-};
+// ArmavatorManualBehaviour::ArmavatorManualBehaviour(Armavator *armavator, frc::XboxController &codriver)
+// : _armavator(armavator), _codriver(codriver) {
+//   //tells code that the points are controlled (one point at a time) 
+//   _setpoint.height = 0.0_m;
+//   _setpoint.angle = 0.0_deg;
+//   Controls(armavator);
+// };
 
-void ArmavatorManualBehaviour::OnStart() {
+// void ArmavatorManualBehaviour::OnStart() {
 
-}
+// }
 
-void ArmavatorManualBehaviour::OnTick(units::second_t dt) {
-  // Manual Positioning
-  // Elevator
-  if (std::abs(_codriver.GetRightY()) > 0.15) {
-    _setpoint.height = _setpoint.height + (_codriver.GetRightY() * 1.0_m);
-  }
+// void ArmavatorManualBehaviour::OnTick(units::second_t dt) {
+//   // Manual Positioning
+//   // Elevator
+//   if (std::abs(_codriver.GetRightY()) > 0.15) {
+//     _setpoint.height = _setpoint.height + (_codriver.GetRightY() * 1.0_m);
+//   }
 
-  // Angle
-  if(std::abs(_codriver.GetLeftY()) > 0.15) {
-    _setpoint.angle = _setpoint.angle + (_codriver.GetLeftY() * 1.0_deg);
-  }
+//   // Angle
+//   if(std::abs(_codriver.GetLeftY()) > 0.15) {
+//     _setpoint.angle = _setpoint.angle + (_codriver.GetLeftY() * 1.0_deg);
+//   }
 
-  // Set Position
-  _armavator->SetPosition(_setpoint);
+//   // Set Position
+//   _armavator->SetPosition(_setpoint);
 
-  if(!_codriver.GetAButton() && !_codriver.GetBButton() && !_codriver.GetXButton() && !_codriver.GetYButton()) {
-    units::volt_t voltage{0};
-  } else {
-    if(_codriver.GetAButton()) {
-      _armavator->SetPosition({0.2_m, 0_deg});
-    }
-    if(_codriver.GetBButton()) {
-      _armavator->SetPosition({1.2_m, 75_deg});
-    }
-    if(_codriver.GetXButton()) {
-      _armavator->SetPosition({1.0_m, 240_deg});
-    }
-    if(_codriver.GetYButton()) {
-      _armavator->SetPosition({0_m, 0_deg});
-    }
-  }
-}
+//   if(!_codriver.GetAButton() && !_codriver.GetBButton() && !_codriver.GetXButton() && !_codriver.GetYButton()) {
+//     units::volt_t voltage{0};
+//   } else {
+//     if(_codriver.GetAButton()) {
+//       _armavator->SetPosition({0.2_m, 0_deg});
+//     }
+//     if(_codriver.GetBButton()) {
+//       _armavator->SetPosition({1.2_m, 75_deg});
+//     }
+//     if(_codriver.GetXButton()) {
+//       _armavator->SetPosition({1.0_m, 240_deg});
+//     }
+//     if(_codriver.GetYButton()) {
+//       _armavator->SetPosition({0_m, 0_deg});
+//     }
+//   }
+// }
 
 ArmavatorRawBehaviour::ArmavatorRawBehaviour(Armavator *armavator, frc::XboxController &codriver)
 : _armavator(armavator), _codriver(codriver) {
--- src/main/include/behaviour/ArmavatorBehaviour.h
+++ src/main/include/behaviour/ArmavatorBehaviour.h
@@ -27,21 +27,21 @@ class ArmavatorGoToPositionBehaviour : public behaviour::Behaviour {
    std::deque<grid_t::GridPathNode<units::second>> _waypoints;
 };
 
-class ArmavatorManualBehaviour : public behaviour::Behaviour {
- public:
-  using grid_t = ArmavatorConfig::grid_t;
+// class ArmavatorManualBehaviour : public behaviour::Behaviour {
+//  public:
+//   using grid_t = ArmavatorConfig::grid_t;
 
-  ArmavatorManualBehaviour(Armavator *armavator, frc::XboxController &codriver);
+//   ArmavatorManualBehaviour(Armavator *armavator, frc::XboxController &codriver);
 
-  void OnStart() override;
-  void OnTick(units::second_t dt) override;
- private:
-  Armavator *_armavator;
+//   void OnStart() override;
+//   void OnTick(units::second_t dt) override;
+//  private:
+//   Armavator *_armavator;
 
-  ArmavatorPosition _setpoint;
-  std::deque<grid_t::GridPathNode<units::second>> _waypoints;
-  frc::XboxController &_codriver;
-};
+//   ArmavatorPosition _setpoint;
+//   std::deque<grid_t::GridPathNode<units::second>> _waypoints;
+//   frc::XboxController &_codriver;
+// };
 
 class ArmavatorRawBehaviour : public behaviour::Behaviour {
  public:
