a2ce32cc31e1a6708e5454750a924eae2fdabdb4
Isaac Turner <skankhunt49@outlook.com.au>
2023-03-10 09:36:43 +0800

    intial commit for vision editing

--- src/main/include/Vision.h
+++ src/main/include/Vision.h
@@ -6,11 +6,23 @@
 
 #include <frc/apriltag/AprilTagFieldLayout.h>
 
+// pp stands for photonpipeline in variable naming
+
 struct VisionConfig {
-  std::shared_ptr<photonlib::PhotonCamera> camera;
+  //current master version of camera declaration
+  // std::shared_ptr<photonlib::PhotonCamera> camera;
+
+  //current version
+  // both declaration versions work but you need to use -> for std pointer
+  photonlib::PhotonCamera camera;
+
+  //untouched
   units::radian_t fov;
   frc::Pose3d robotToCamera;
   std::shared_ptr<frc::AprilTagFieldLayout> layout;
+
+  //new other things
+  photonlib::PhotonPipelineResult ppResult = camera.GetLatestResult();
 };
 
 std::shared_ptr<frc::AprilTagFieldLayout> Get2023Layout();
