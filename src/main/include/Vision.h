5d6841c3e944bd0006e86752bee0d94cb748604a
Isaac Turner <skankhunt49@outlook.com.au>
2023-03-28 11:24:24 +0800

    ignore this branch forever

--- src/main/include/Vision.h
+++ src/main/include/Vision.h
@@ -1,97 +1,97 @@
 #pragma once
 
-#include "behaviour/HasBehaviour.h"
+// #include "behaviour/HasBehaviour.h"
 
-#include <frc/geometry/Transform3d.h>
-#include <photonlib/PhotonCamera.h>
-#include <photonlib/RobotPoseEstimator.h>
-#include <photonlib/SimPhotonCamera.h>
-// // #include "drivetrain/SwerveDrive.h"
+// #include <frc/geometry/Transform3d.h>
+// #include <photonlib/PhotonCamera.h>
+// #include <photonlib/RobotPoseEstimator.h>
+// #include <photonlib/SimPhotonCamera.h>
+// // // #include "drivetrain/SwerveDrive.h"
 
-<<<<<<< HEAD
-#include <frc/geometry/Transform3d.h>
-#include <frc/geometry/Pose3d.h>
-#include <photonlib/PhotonCamera.h>
-#include <photonlib/SimPhotonCamera.h>
-#include <photonlib/RobotPoseEstimator.h>
-#include <NTUtil.h>
-#include <wpi/json.h>
-#include <frc/apriltag/AprilTagFields.h>
-#include <frc/apriltag/AprilTagFieldLayout.h>
-#include <frc/geometry/Transform3d.h>
+// <<<<<<< HEAD
+// #include <frc/geometry/Transform3d.h>
+// #include <frc/geometry/Pose3d.h>
+// #include <photonlib/PhotonCamera.h>
+// #include <photonlib/SimPhotonCamera.h>
+// #include <photonlib/RobotPoseEstimator.h>
+// #include <NTUtil.h>
+// #include <wpi/json.h>
+// #include <frc/apriltag/AprilTagFields.h>
+// #include <frc/apriltag/AprilTagFieldLayout.h>
+// #include <frc/geometry/Transform3d.h>
 
-using namespace photonlib;
+// using namespace photonlib;
 
-std::shared_ptr<frc::AprilTagFieldLayout> Get2023Layout() {
-  return std::make_shared<frc::AprilTagFieldLayout>(frc::LoadAprilTagLayoutField(frc::AprilTagField::k2023ChargedUp));
-};
+// std::shared_ptr<frc::AprilTagFieldLayout> Get2023Layout() {
+//   return std::make_shared<frc::AprilTagFieldLayout>(frc::LoadAprilTagLayoutField(frc::AprilTagField::k2023ChargedUp));
+// };
 
-// pp stands for photonpipeline in variable naming
+// // pp stands for photonpipeline in variable naming
 
-struct VisionConfig {
-  std::shared_ptr<PhotonCamera> camera;
+// struct VisionConfig {
+//   std::shared_ptr<PhotonCamera> camera;
 
-  units::radian_t fov;
-  frc::Transform3d robotToCamera; // assuming this means centre of robot to camera, but UNSURE
-  std::shared_ptr<frc::AprilTagFieldLayout> layout;
-};
+//   units::radian_t fov;
+//   frc::Transform3d robotToCamera; // assuming this means centre of robot to camera, but UNSURE
+//   std::shared_ptr<frc::AprilTagFieldLayout> layout;
+// };
 
-class Vision {
-  private :
-    VisionConfig defaultConfig;
-    VisionConfig visionConfig;
-    RobotPoseEstimator _estimator;
-  public :
-    // Vision(VisionConfig config, RobotPoseEstimator _estimator);
-    Vision(VisionConfig config/*, RobotPoseEstimator _estimator*/);
+// class Vision {
+//   private :
+//     VisionConfig defaultConfig;
+//     VisionConfig visionConfig;
+//     RobotPoseEstimator _estimator;
+//   public :
+//     // Vision(VisionConfig config, RobotPoseEstimator _estimator);
+//     Vision(VisionConfig config/*, RobotPoseEstimator _estimator*/);
     
-    void OnUpdate(units::second_t dt); 
+//     void OnUpdate(units::second_t dt); 
 
-    PhotonPipelineResult getLatestResults(std::shared_ptr<PhotonCamera> camera) {
-      PhotonPipelineResult ppResult = camera->GetLatestResult();
-      return ppResult;
-    };
+//     PhotonPipelineResult getLatestResults(std::shared_ptr<PhotonCamera> camera) {
+//       PhotonPipelineResult ppResult = camera->GetLatestResult();
+//       return ppResult;
+//     };
 
-    auto getTargets(std::shared_ptr<PhotonCamera> camera) {
-      PhotonPipelineResult ppResults = getLatestResults(visionConfig.camera);
-      auto targets = ppResults.GetTargets();
-      return targets;
-    };
+//     auto getTargets(std::shared_ptr<PhotonCamera> camera) {
+//       PhotonPipelineResult ppResults = getLatestResults(visionConfig.camera);
+//       auto targets = ppResults.GetTargets();
+//       return targets;
+//     };
 
-  PhotonTrackedTarget getBestTarget(std::shared_ptr<PhotonCamera> camera, PhotonPipelineResult result) {
-    PhotonTrackedTarget bestTarget = result.GetBestTarget();
-    return bestTarget;
-  };
+//   PhotonTrackedTarget getBestTarget(std::shared_ptr<PhotonCamera> camera, PhotonPipelineResult result) {
+//     PhotonTrackedTarget bestTarget = result.GetBestTarget();
+//     return bestTarget;
+//   };
 
-  auto estimatePose(VisionConfig config) {
-    visionConfig = config;
-    _estimator = RobotPoseEstimator{
-      visionConfig.layout,
-      photonlib::AVERAGE_BEST_TARGETS,
-      {std::make_pair(config.camera, config.robotToCamera)}
-    };
+//   auto estimatePose(VisionConfig config) {
+//     visionConfig = config;
+//     _estimator = RobotPoseEstimator{
+//       visionConfig.layout,
+//       photonlib::AVERAGE_BEST_TARGETS,
+//       {std::make_pair(config.camera, config.robotToCamera)}
+//     };
 
-    std::pair<frc::Pose3d, units::millisecond_t> pose_result = _estimator.Update();
-    auto table = nt::NetworkTableInstance::GetDefault().GetTable("Vision");
-    wom::WritePose3NT(table, pose_result.first);
-    return pose_result.first;
-  };
+//     std::pair<frc::Pose3d, units::millisecond_t> pose_result = _estimator.Update();
+//     auto table = nt::NetworkTableInstance::GetDefault().GetTable("Vision");
+//     wom::WritePose3NT(table, pose_result.first);
+//     return pose_result.first;
+//   };
 
-  auto getPathForBest(std::shared_ptr<PhotonCamera> camera) {
-    PhotonPipelineResult ppResults = getLatestResults(camera);
-    PhotonTrackedTarget bestTarget = getBestTarget(camera, ppResults);
-    frc::Pose3d poseEstimate = estimatePose(defaultConfig);
-    frc::Transform3d relativeBestTargetPose = bestTarget.GetBestCameraToTarget(); // relative position of target (treating robot position as (0, 0, 0, 0))
-    frc::Pose2d bestTargetPose = frc::Pose2d{
-      frc::Translation2d{poseEstimate.X() + relativeBestTargetPose.X(), poseEstimate.Y() + relativeBestTargetPose.Y()},
-      poseEstimate.Rotation().ToRotation2d()
-    };
-    return bestTargetPose;
-  };
-};
+//   auto getPathForBest(std::shared_ptr<PhotonCamera> camera) {
+//     PhotonPipelineResult ppResults = getLatestResults(camera);
+//     PhotonTrackedTarget bestTarget = getBestTarget(camera, ppResults);
+//     frc::Pose3d poseEstimate = estimatePose(defaultConfig);
+//     frc::Transform3d relativeBestTargetPose = bestTarget.GetBestCameraToTarget(); // relative position of target (treating robot position as (0, 0, 0, 0))
+//     frc::Pose2d bestTargetPose = frc::Pose2d{
+//       frc::Translation2d{poseEstimate.X() + relativeBestTargetPose.X(), poseEstimate.Y() + relativeBestTargetPose.Y()},
+//       poseEstimate.Rotation().ToRotation2d()
+//     };
+//     return bestTargetPose;
+//   };
+// };
 
-std::shared_ptr<frc::AprilTagFieldLayout> Get2023Layout();
-=======
+// std::shared_ptr<frc::AprilTagFieldLayout> Get2023Layout();
+// =======
 #include "behaviour/HasBehaviour.h"
 
 #include <frc/geometry/Transform3d.h>
@@ -178,16 +178,3 @@ class Vision {
 };
 
 std::shared_ptr<frc::AprilTagFieldLayout> Get2023Layout();
-
-class Vision : public behaviour::HasBehaviour {
-  public:
-    Vision(VisionConfig *config);
-    std::optional<std::pair<frc::Pose3d, units::second_t>> OnUpdate(units::second_t dt);   
-
-    void OnTick(units::second_t dt); 
-
-  private:
-    VisionConfig *_config;
-    photonlib::RobotPoseEstimator _estimator;
-};
->>>>>>> f473d620c7e327dfeb37757cc5a8a3d6c34ac45c
