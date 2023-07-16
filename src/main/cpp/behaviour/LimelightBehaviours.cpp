d2175d0421a77b6d89625cbf3212be2e63506ac0
Isaac Turner <spacey_sooty@outlook.com>
2023-07-16 16:38:49 +0800

    added behaviour files but i had an idea

---
+++ src/main/cpp/behaviour/LimelightBehaviours.cpp
@@ -0,0 +1,2 @@
+#include "behaviour/LimelightBehaviours.h"
+
---
+++ src/main/include/behaviour/LimelightBehaviours.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// Local includes
+#include "LimeLightVision.h"
+
+// wom includes
+#include "behaviour/Behaviour.h"
+
+// std lib includes
+
+// units includes
+
+// wpilib includes
+
+// other external includes
+
