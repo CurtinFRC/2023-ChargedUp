d182b748d2f8b14251a95063a013cee75a1643ac
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2023-08-13 15:39:05 +0800

    Added auto align

--- src/main/cpp/behaviour/LimelightBehaviours.cpp
+++ src/main/cpp/behaviour/LimelightBehaviours.cpp
@@ -1,2 +1,40 @@
 #include "behaviour/LimelightBehaviours.h"
 
+
+AlignBehaviour::AlignBehaviour(Limelight *limelight, frc::XboxController &driver, wom::SwerveDrive *swerve)
+    : _limelight(limelight), _driver(driver), _swerve(swerve) {
+  Controls(limelight);
+  Controls(swerve);
+}
+
+void AlignBehaviour::AlignToTarget(frc::Pose3d pose) {
+  //swerve->SetPose(pose);
+
+  _state = poseState::notAtPose;
+  _setPoint = pose;
+}
+
+void AlignBehaviour::CancelAlign() {
+  _state = poseState::atPose;
+}
+
+void AlignBehaviour::OnTick(units::second_t dt) {
+    if (_driver.GetAButton()) {
+        AlignToTarget(_SetPoints[0]);
+    } else if (_driver.GetXButton()) {
+        AlignToTarget(_SetPoints[1]);
+    } else if (_driver.GetYButton()) {
+        AlignToTarget(_SetPoints[2]);
+    } else if (_driver.GetBButton()) {
+        AlignToTarget(_SetPoints[3]);
+    } else if (_driver.GetBackButtonPressed()) {
+        CancelAlign();
+    }
+
+    if (_state == poseState::notAtPose && !_limelight->IsAtSetPoseVision(_setPoint, dt)) {
+        _swerve->SetPose(_setPoint.ToPose2d());
+    }
+    else {
+        _state = poseState::atPose;
+    }
+}
